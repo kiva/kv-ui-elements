964d471ef73a3ebea3b43f45e545db98c1993d82
Ryan Ludwig <ryanl@kiva.org>
2021-04-13 10:11:01 -0700

    Initial scaffolding of storybook + tailwind

---
+++ kv-components/vue/.storybook/main.js
@@ -0,0 +1,33 @@
+const path = require('path');
+
+module.exports = {
+  stories: [
+    '../stories/**/*.stories.mdx',
+    '../stories/**/*.stories.@(js|jsx|ts|tsx)'
+  ],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+	'@storybook/addon-a11y',
+	'@storybook/addon-storysource'
+  ],
+  webpackFinal: async (config) => {
+    config.module.rules.push({
+      test: /\,css&/,
+      use: [
+        {
+          loader: 'postcss-loader',
+          options: {
+            ident: 'postcss',
+            plugins: [
+              require('tailwindcss'),
+              require('autoprefixer')
+            ]
+          }
+        }
+      ],
+      include: path.resolve(__dirname, '../'),
+    })
+    return config
+  }
+}
---
+++ kv-components/vue/.storybook/preview.js
@@ -0,0 +1,13 @@
+import '../../../kv-tokens/configs/tailwind.css';
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+}
+
+
