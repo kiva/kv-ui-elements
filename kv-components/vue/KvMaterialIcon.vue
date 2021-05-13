007ceeaba9d011d1e358bd9a6cbc79e7a1da5c55
Ryan Ludwig <ryanl@kiva.org>
2021-05-12 17:18:53 -0700

    feat(KvMaterialIcon): New component for displaying Material Design icons

---
+++ kv-components/vue/KvMaterialIcon.vue
@@ -0,0 +1,31 @@
+<template>
+	<span
+		class="inline-flex"
+		aria-hidden="true"
+		role="img"
+	>
+		<svg
+			class="w-full h-full fill-current"
+			width="24"
+			height="24"
+			viewBox="0 0 24 24"
+		>
+			<path :d="icon" />
+		</svg>
+	</span>
+</template>
+
+<script>
+export default {
+	props: {
+		/**
+		 * SVG path data passed in from an imported @mdi/js module.
+		 * E.g., import { mdiAccount } from '@mdi/js'; <kv-material-icon :icon="mdiAccount" />
+		 * */
+		icon: {
+			type: String,
+			default: '',
+		},
+	},
+};
+</script>
