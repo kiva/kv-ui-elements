220b6cb774ca42ffc2be650e48ff7ddcada9dc1a
Joshua <joshuadstarkey@gmail.com>
2021-05-13 21:45:09 -0700

    VUE-592 props in place, label variant built and styled

--- kv-components/vue/KvSelect.vue
+++ kv-components/vue/KvSelect.vue
@@ -1,12 +1,22 @@
 <template>
-	<component
-		:is="tag"
-		v-bind="$attrs"
-		class="text-base h-6 pl-1 border border-gray-300 rounded-sm"
-		@click="onClick"
-	>
-		<slot></slot>
-	</component>
+	<div>
+		<label
+			:v-if="id"
+			:for="id"
+			class="block uppercase text-small"
+		>
+			{{ label }}
+		</label>
+		<select
+			:id="id"
+			:disabled="disabled"
+			:width="width"
+			:class="`${id ? 'mt-2' : ''} text-base h-6 pl-1 border border-gray-300 rounded-sm`"
+			@change="onChange"
+		>
+			<slot></slot>
+		</select>
+	</div>
 </template>
 
 <script>
@@ -16,27 +26,37 @@ export default {
 		//   type: String,
 		//   default: "",
 		// },
+		label: {
+			type: String,
+			default: '',
+		},
+		disabled: {
+			type: Boolean,
+			default: false,
+		},
+		id: {
+			type: String,
+			required: false,
+			default: '',
+		},
+		width: {
+			// string due to class being set with tailwinds class
+			type: String,
+			default: '',
+		},
 	},
 	data() {
 		return {
 			inputValue: null,
 		};
 	},
-	computed: {
-		// Maybe this tag is unnecessary and could just be
-		// hardcoded to select on line 5?
-		tag() {
-			return 'select';
-		},
-	},
 	methods: {
-		onClick(e) {
-			if (this.tag === 'select' && this.$attrs.type !== 'submit') {
-				// emit a vue event and prevent native event
-				// so we don't have to write @click.native in our templates
-				e.preventDefault();
-				this.$emit('click', e);
-			}
+		onChange(event) {
+			// emit a vue event and prevent native event
+			// so we don't have to write @click.native in our templates
+			event.preventDefault();
+			this.$emit('change', event);
+			console.log('onChange triggered.');
 		},
 	},
 };
--- kv-components/vue/stories/KvSelect.stories.js
+++ kv-components/vue/stories/KvSelect.stories.js
@@ -11,11 +11,14 @@ export const Default = () => ({
 		exampleModel: 'example3',
 	}),
 	template: `
-		<kv-select v-model="exampleModel">
-			<option class="px-3 py-2" value="example1">Example one</option>
-			<option class="px-3 py-2" value="example2">Example two</option>
-			<option class="px-3 py-2" value="example3">Example three</option>
-		</kv-select>
+		<div>
+			<label></label>
+			<kv-select v-model="exampleModel">
+				<option class="px-3 py-2" value="example1">Example one</option>
+				<option class="px-3 py-2" value="example2">Example two</option>
+				<option class="px-3 py-2" value="example3">Example three</option>
+			</kv-select>
+		</div>
 	`,
 });
 
@@ -27,10 +30,59 @@ export const Disabled = () => ({
 		exampleModel: 'example2',
 	}),
 	template: `
-		<kv-select disabled v-model="exampleModel">
-			<option value="example1">Example one</option>
-			<option value="example2">Example two</option>
-			<option value="example3">Example three</option>
-		</kv-select>
+		<div>
+			<label></label>
+			<kv-select disabled v-model="exampleModel">
+				<option value="example1">Example one</option>
+				<option value="example2">Example two</option>
+				<option value="example3">Example three</option>
+			</kv-select>
+		</div>
+	`,
+});
+
+export const withLabel = () => ({
+	components: {
+		KvSelect,
+	},
+	data: () => ({
+		exampleModel: 'example1',
+	}),
+	props: {
+		id: {
+			type: String,
+			default: 'thisDropdown',
+		},
+		labelText: {
+			type: String,
+			default: 'This is a label',
+		},
+	},
+	template: `
+		<div>
+			<label :for="id" class="uppercase text-small pb-4">{{ labelText }}</label>
+			<kv-select :id="id" v-model="exampleModel" class="mt-2">
+				<option value="example1">Example one</option>
+				<option value="example2">Example two</option>
+				<option value="example3">Example three</option>
+			</kv-select>
+		</div>
+	`,
+});
+
+export const withSetWidth = () => ({
+	components: { KvSelect },
+	data: () => ({
+		exampleModel: 'example3',
+	}),
+	template: `
+		<div>
+			<label></label>
+			<kv-select :width="" v-model="exampleModel">
+				<option class="px-3 py-2" value="example1">Example one</option>
+				<option class="px-3 py-2" value="example2">Example two</option>
+				<option class="px-3 py-2" value="example3">Example three</option>
+			</kv-select>
+		</div>
 	`,
 });
