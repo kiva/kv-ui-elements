ed1239f21d06c09212f96b8a23c8a8865f993780
Ryan Ludwig <ryanl@kiva.org>
2021-05-14 10:09:41 -0700

    docs(KvMaterialIcon): Add usage documentation

--- kv-components/vue/KvMaterialIcon.vue
+++ kv-components/vue/KvMaterialIcon.vue
@@ -16,11 +16,27 @@
 </template>
 
 <script>
+/**
+ * This component takes path data as a prop from an imported @mdi/js module and
+ * renders the icon as an SVG.
+ *
+ * Find your icon name from https://materialdesignicons.com/,
+ * The module name to import will be 'mdiPascalCaseIconName'. `account-cowboy-hat` would be
+ * `import { mdiAccountCowboyHat } from '@mdi/js'`;
+ *
+ * These icons do not announce to screenreaders. If you are using a non-decorative icon, be sure
+ * to include screen reader text. E.g.,
+ * `<button>
+ * 	  <kv-material-icon :icon="mdiClose" />
+ * 	  <span class="sr-only">Close modal</span>
+ *  </button>`
+ */
+
 export default {
 	props: {
 		/**
 		 * SVG path data passed in from an imported @mdi/js module.
-		 * E.g., import { mdiAccount } from '@mdi/js'; <kv-material-icon :icon="mdiAccount" />
+		 * E.g., `import { mdiAccount } from '@mdi/js'; <kv-material-icon :icon="mdiAccount" />`
 		 * */
 		icon: {
 			type: String,
