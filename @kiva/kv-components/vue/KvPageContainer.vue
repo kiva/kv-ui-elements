d007e1b3e1bb770782fe84f78802cdbdbf0e8fd4
Ryan Ludwig <ryanl@kiva.org>
2021-06-16 15:11:15 -0700

    feat(KvPageContainer): A component that adds margins, centers and contains content per design specs

---
+++ @kiva/kv-components/vue/KvPageContainer.vue
@@ -0,0 +1,15 @@
+<template>
+	<div
+		class="tw-mx-auto tw-px-2.5 md:tw-px-4 lg:tw-px-8"
+		style="max-width: 1200px;"
+	>
+		<slot></slot>
+	</div>
+</template>
+
+<script>
+/**
+ * Wrapper component which contains content to the specified widths and margins set by design
+ */
+export default {};
+</script>
---
+++ @kiva/kv-components/vue/stories/KvPageContainer.stories.js
@@ -0,0 +1,50 @@
+import KvPageContainer from '../KvPageContainer.vue';
+
+export default {
+	title: 'KvPageContainer',
+	component: KvPageContainer,
+	parameters: {
+		layout: 'fullscreen',
+	},
+};
+
+const DefaultTemplate = () => ({
+	components: { KvPageContainer },
+	template: `
+		<div class="demo">
+			<component is="style">
+				.demo * { outline: 1px solid red !important; }
+			</component>
+
+			<kv-page-container>
+				<p>I'm constrained!</p>
+			</kv-page-container>
+		</div>`,
+});
+
+const BreakingOutTemplate = () => ({
+	components: { KvPageContainer },
+	template: `
+		<div class="demo">
+			<component is="style">
+				.demo * { outline: 1px solid red !important; }
+			</component>
+
+			<div>
+				<kv-page-container>
+					<p>I'm constrained!</p>
+				</kv-page-container>
+
+				<img
+					class="tw-w-full"
+					src="https://via.placeholder.com/2000x400">
+
+				<kv-page-container>
+					<p>I'm constrained!</p>
+				</kv-page-container>
+			</div>
+		</div>`,
+});
+
+export const Default = DefaultTemplate.bind({});
+export const BreakingOut = BreakingOutTemplate.bind({});
