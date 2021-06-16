3886aff12d185adb2f921f7d37676236dde9066e
Ryan Ludwig <ryanl@kiva.org>
2021-06-16 15:13:20 -0700

    feat(KvGrid): A component that sets the grid gaps as specified by design

---
+++ @kiva/kv-components/vue/KvGrid.vue
@@ -0,0 +1,14 @@
+<template>
+	<div class="tw-grid tw-gap-x-2 md:tw-gap-x-3 lg:tw-gap-x-3.5">
+		<slot></slot>
+	</div>
+</template>
+
+<script>
+/**
+ * A light wrapper around Tailwind's grid class, setting the gaps as specified by design.
+ * To use, specify the number of columns as a tailwind class on this component.
+ * e.g., `<kv-grid class="tw-grid-cols-6 md:tw-grid-cols-12"> grid children here... </kv-grid>`
+ */
+export default {};
+</script>
