aafd32b9f8d1554157ffe4d50a8335cce4db7ca2
Ryan Ludwig <ryanl@kiva.org>
2021-07-09 12:25:52 -0700

    build: use Tailwind's JIT mode

--- @kiva/kv-components/tailwind.config.js
+++ @kiva/kv-components/tailwind.config.js
@@ -1,5 +1,11 @@
 const sharedConfig = require('@kiva/kv-tokens/configs/tailwind.config');
 
 module.exports = {
+	mode: 'jit',
 	presets: [sharedConfig],
+	purge: [
+		'./vue/**/*.vue',
+		'./vue/stories/**/*.stories.js',
+		'./utils/**/*.js',
+	],
 };
