4303abf4e091d7afb30d4c02e31d6b6680cc4094
Ryan Ludwig <ryanl@kiva.org>
2021-08-05 13:47:08 -0700

    docs: Add a form kitchen sink story

---
+++ @kiva/kv-components/vue/stories/Forms.stories.js
@@ -0,0 +1,50 @@
+import KvCheckbox from '../KvCheckbox.vue';
+import KvRadio from '../KvRadio.vue';
+import KvSelect from '../KvSelect.vue';
+import KvSwitch from '../KvSwitch.vue';
+
+import { Multiple as CheckboxDefault } from './KvCheckbox.stories';
+import { Default as RadioDefault } from './KvRadio.stories';
+import { Default as SelectDefault } from './KvSelect.stories';
+import { Default as SwitchDefault } from './KvSwitch.stories';
+
+export default {
+	title: 'Forms',
+	args: {
+		checked: false,
+		disabled: false,
+	},
+};
+
+const Template = (args, {
+	argTypes,
+}) => ({
+	props: Object.keys(argTypes),
+	components: {
+		KvCheckbox,
+		KvRadio,
+		KvSelect,
+		KvSwitch,
+	},
+	data: () => ({
+		...CheckboxDefault(args, { argTypes }).data(),
+		...RadioDefault(args, { argTypes }).data(),
+		...SwitchDefault(args, { argTypes }).data(),
+		...SelectDefault(args, { argTypes }).data(),
+	}),
+	methods: {
+		...CheckboxDefault(args, { argTypes }).methods,
+		...RadioDefault(args, { argTypes }).methods,
+		...SwitchDefault(args, { argTypes }).methods,
+		...SelectDefault(args, { argTypes }).methods,
+	},
+	template: `
+		<div>
+			${CheckboxDefault(args, { argTypes }).template}<br /><br />
+			${RadioDefault(args, { argTypes }).template}<br /><br />
+			${SwitchDefault(args, { argTypes }).template}<br /><br />
+			${SelectDefault(args, { argTypes }).template}<br /><br />
+		</div>`,
+});
+
+export const KitchenSink = Template.bind({});
