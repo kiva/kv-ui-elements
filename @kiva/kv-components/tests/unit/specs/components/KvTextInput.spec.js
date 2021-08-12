9c75c286dd705696f6a86f1fd2bce9e951d0d033
Ryan Ludwig <ryanl@kiva.org>
2021-08-12 15:41:49 -0700

    test(KvTextInput): Add tests

--- @kiva/kv-components/tests/unit/specs/components/KvTextInput.spec.js
+++ @kiva/kv-components/tests/unit/specs/components/KvTextInput.spec.js
@@ -1,4 +1,5 @@
 import { render } from '@testing-library/vue';
+import userEvent from '@testing-library/user-event';
 import { axe, toHaveNoViolations } from 'jest-axe';
 import KvTextInput from '../../../../vue/KvTextInput.vue';
 
@@ -18,48 +19,38 @@ describe('KvTextInput', () => {
 	it('renders with a role of "textbox"', () => {
 		const { getByRole } = render(KvTextInputTemplate);
 		const textInputEl = getByRole('textbox');
+
 		expect(textInputEl).toBeDefined();
 	});
 
-	// it('toggles the switch when the label is clicked', async () => {
-	// 	const { getByLabelText } = render(KvTextInput, {
-	// 		slots: { default: 'Test Switch' },
-	// 	});
-	// 	const textInputEl = getByLabelText('Test Switch');
+	it('updates the value when text is entered', async () => {
+		const { getByRole } = render(KvTextInputTemplate);
+		const textInputEl = getByRole('textbox');
 
-	// 	expect(textInputEl.checked).toEqual(false);
-	// 	await textInputEl.click();
-	// 	expect(textInputEl.checked).toEqual(true);
-	// });
+		expect(textInputEl.value).toEqual('');
+		await userEvent.type(textInputEl, 'abc 123');
+		expect(textInputEl.value).toEqual('abc 123');
+	});
 
-	// it('can\'t be input when the disabled prop is true', async () => {
-	// 	const { getByRole } = render(KvTextInput, {
-	// 		props: { disabled: true },
-	// 	});
-	// 	const textInputEl = getByRole('textbox');
+	it('can\'t be input when the disabled prop is true', async () => {
+		const { getByRole } = render(KvTextInput, {
+			props: {
+				disabled: true,
+				id: 'foo',
+			},
+		});
+		const textInputEl = getByRole('textbox');
 
-	// 	expect(textInputEl.value).toEqual('');
-	// 	await textInputEl.click();
-	// 	expect(textInputEl.checked).toEqual(false);
-	// });
-
-	// it('emits a input event when text is entered', async () => {
-	// 	const { getByLabelText, emitted } = render(KvTextInput, {
-	// 		slots: { default: 'Test Switch' },
-	// 	});
-	// 	const textInputEl = getByLabelText('Test Switch');
-
-	// 	await textInputEl.click();
-	// 	expect(emitted().change[0]).toEqual([true]);
-
-	// 	await textInputEl.click();
-	// 	expect(emitted().change[1]).toEqual([false]);
-	// 	expect(emitted().change.length).toBe(2);
-	// });
+		expect(textInputEl.disabled).toBeTruthy();
+		expect(textInputEl.value).toEqual('');
+		await userEvent.type(textInputEl, 'abc 123');
+		expect(textInputEl.value).toEqual('');
+	});
 
 	it('has no automated accessibility violations', async () => {
 		const { container } = render(KvTextInputTemplate);
 		const results = await axe(container);
+
 		expect(results).toHaveNoViolations();
 	});
 });
