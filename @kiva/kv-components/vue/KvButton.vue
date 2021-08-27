b0f97c346b6af3686a76e86450307d40065c9b66
Ryan Ludwig <ryanl@kiva.org>
2021-08-27 11:24:31 -0700

    Fix blip color

--- @kiva/kv-components/vue/KvButton.vue
+++ @kiva/kv-components/vue/KvButton.vue
@@ -170,7 +170,7 @@ export default {
 
 			// some variants shouldn't have a white blip
 			const darkBlipVariants = ['secondary', 'ghost'];
-			const blipBgColor = darkBlipVariants.includes(this.variant) ? 'tw-bg-tertiary' : 'tw-bg-white';
+			const blipBgColor = darkBlipVariants.includes(this.variant) ? 'tw-bg-tertiary' : 'tw-bg-primary';
 			blipEl.classList.add(blipBgColor);
 
 			// position the blip where the pointer click is or center it if keyboard
