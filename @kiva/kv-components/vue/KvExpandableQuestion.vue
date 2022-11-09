8f629d7659aec83493797e8b1bb9d4540457686a
Matt Stover <mcs@mcstover.com>
2022-11-08 17:21:05 -0800

    fix: smooth out transition by apply styling to child div

--- @kiva/kv-components/vue/KvExpandableQuestion.vue
+++ @kiva/kv-components/vue/KvExpandableQuestion.vue
@@ -14,15 +14,14 @@
 			/>
 		</button>
 		<kv-expandable easing="ease-in-out">
-			<div
-				v-show="open"
-				class="tw-prose tw-pb-4 tw-pt-2"
-			>
-				<slot></slot>
-				<div
-					v-if="content !== ''"
-					v-html="content"
-				>
+			<div v-show="open">
+				<div class="tw-prose tw-pb-4 tw-pt-2">
+					<slot></slot>
+					<div
+						v-if="content !== ''"
+						v-html="content"
+					>
+					</div>
 				</div>
 			</div>
 		</kv-expandable>
