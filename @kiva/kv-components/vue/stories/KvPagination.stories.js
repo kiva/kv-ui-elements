5f24d6aae4bdb2909fb61c666b6a3d64dd76da99
christian14b <christian.bedon@contractors.kiva.org>
2023-04-13 19:35:53 -0500

    feat: move KvPagination component to kv-elements

---
+++ @kiva/kv-components/vue/stories/KvPagination.stories.js
@@ -0,0 +1,68 @@
+import KvPagination from '../KvPagination.vue';
+
+export default {
+	title: 'KvPagination',
+	component: KvPagination,
+};
+
+const story = (args) => {
+	const template = (_args, { argTypes }) => ({
+		props: Object.keys(argTypes),
+		components: { KvPagination },
+		template: `<kv-pagination
+			:limit="limit"
+			:total="total"
+			:offset="offset"
+			:extra-pages="extraPages"
+			:kv-track-function="kvTrackMock"
+			:track-event-category="trackEventCategory"
+			/>`,
+		methods: {
+			kvTrackMock(
+				category,
+				action,
+				label,
+			) {
+				console.log(category, action, label);
+			},
+		},
+	});
+	template.args = args;
+	return template;
+};
+
+export const Default = story({ limit: 10, total: 0, trackEventCategory: 'blog-articles' });
+
+export const FewerPages = story({ limit: 10, total: 30, trackEventCategory: 'blog-articles' });
+
+export const MorePages = story({ limit: 10, total: 1000, trackEventCategory: 'blog-articles' });
+
+export const SecondSelected = story({
+	limit: 10, total: 1000, offset: 10, trackEventCategory: 'blog-articles',
+});
+
+export const ThirdSelected = story({
+	limit: 10, total: 1000, offset: 20, trackEventCategory: 'blog-articles',
+});
+
+export const FourthSelected = story({
+	limit: 10, total: 1000, offset: 30, trackEventCategory: 'blog-articles',
+});
+
+export const LastSelected = story({
+	limit: 10, total: 1000, offset: 990, trackEventCategory: 'blog-articles',
+});
+
+export const SecondToLastSelected = story({
+	limit: 10, total: 1000, offset: 980, trackEventCategory: 'blog-articles',
+});
+
+export const ThirdToLastSelected = story({
+	limit: 10, total: 1000, offset: 970, trackEventCategory: 'blog-articles',
+});
+
+export const FourthToLastSelected = story({
+	limit: 10, total: 1000, offset: 960, trackEventCategory: 'blog-articles',
+});
+
+export const MoreExtraPages = story({ limit: 10, total: 1000, extraPages: 6 });
