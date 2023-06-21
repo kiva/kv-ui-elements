d7ef9f4749e06e43b28c2bdac6e63bf5329b69a6
Casey Dyer <dyersituations@gmail.com>
2023-06-21 11:34:20 -0700

    fix: tests for previous commit

--- @kiva/kv-loan-filters/src/tests/activities.spec.ts
+++ @kiva/kv-loan-filters/src/tests/activities.spec.ts
@@ -15,5 +15,19 @@ describe('activities.ts', () => {
 				expect(activities.getFlssFilter({ activityId: [1] })).toEqual({ activityId: { any: [1] } });
 			});
 		});
+
+		describe('getValidatedSearchState', () => {
+			it('should handle undefined', () => {
+				expect(activities.getValidatedSearchState(undefined)).toEqual({ activityId: [] });
+			});
+
+			it('should handle missing', () => {
+				expect(activities.getValidatedSearchState({})).toEqual({ activityId: [] });
+			});
+
+			it('should return value', () => {
+				expect(activities.getValidatedSearchState({ activityId: [3] })).toEqual({ activityId: [3] });
+			});
+		});
 	});
 });
