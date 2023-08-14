0d5e028837c0edcedc87092215f0be94b6565996
Roger Gutierrez <94026278+roger-in-kiva@users.noreply.github.com>
2023-08-14 15:35:12 -0600

    feat: avoid fractional page number in kvpaginator component (#281)
    * feat: avoid fractional page number in kvpaginator component
    
    * fix: ceil method added to page number

--- @kiva/kv-components/vue/KvPagination.vue
+++ @kiva/kv-components/vue/KvPagination.vue
@@ -109,7 +109,7 @@ export default {
 	},
 	computed: {
 		current() {
-			const page = this.offset / this.limit;
+			const page = Math.ceil(this.offset / this.limit);
 
 			// This component uses a 0-based page index
 			return page < this.totalPages ? page : 0;
