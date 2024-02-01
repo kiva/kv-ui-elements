8ff657165a9b258dd4996cea4968364dd9a16062
Casey Dyer <dyersituations@gmail.com>
2024-02-01 13:37:37 -0800

    feat: updated activity feed service for getting activities and adding comments

--- @kiva/kv-activity-feed/src/tests/ActivityFeedService.spec.ts
+++ @kiva/kv-activity-feed/src/tests/ActivityFeedService.spec.ts
@@ -1,98 +1,88 @@
+/* eslint-disable no-new */
+import { connect } from 'getstream';
 import ActivityFeedService from '../index';
+import * as parseError from '../utils/parseError';
 
-const mockFollow = jest.fn(() => Promise.resolve());
-const mockAddActivity = jest.fn(() => Promise.resolve());
-const mockGet = jest.fn(() => Promise.resolve({ results: [] }));
+const mockActivity = { id: 'asd' };
 
-const mockFeed = jest.fn(() => ({
-	follow: mockFollow,
-	addActivity: mockAddActivity,
-	get: mockGet,
-}));
+const mockGetActivities = jest.fn(() => Promise.resolve({ results: [mockActivity] }));
+const mockAddComment = jest.fn();
+
+const mockClient = {
+	getActivities: mockGetActivities,
+	reactions: {
+		add: mockAddComment,
+	},
+};
 
 jest.mock('getstream', () => ({
-	connect: jest.fn(() => ({
-		feed: mockFeed,
-	})),
+	connect: jest.fn(() => mockClient),
 }));
 
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const { connect } = require('getstream');
+const API_KEY = 'API_KEY';
+const AUTH_TOKEN = 'AUTH_TOKEN';
+const APP_ID = 'APP_ID';
 
 describe('StreamService', () => {
-	let activityFeedService;
+	let spyParseError: jest.SpyInstance;
 
 	beforeEach(() => {
-		// Reset the mocks and create a new StreamService instance for each test
-		jest.clearAllMocks();
-		activityFeedService = new ActivityFeedService(
-			'API_KEY',
-			'AUTH_TOKEN',
-			'APP_ID',
-		);
+		spyParseError = jest.spyOn(parseError, 'default').mockImplementation(() => {});
 	});
 
-	describe('createUserFeed', () => {
-		it('should create a user feed', () => {
-			const userId = 'user123';
-			const userFeed = activityFeedService.createUserFeed(userId);
+	afterEach(jest.clearAllMocks);
 
-			expect(userFeed).toBeDefined();
-			expect(connect).toHaveBeenCalledWith(
-				'API_KEY',
-				'AUTH_TOKEN',
-				'APP_ID',
-			);
-			expect(connect().feed).toHaveBeenCalledWith('user', userId);
+	describe('constructor', () => {
+		it('should create instance', async () => {
+			new ActivityFeedService(API_KEY, AUTH_TOKEN, APP_ID);
+
+			expect(connect).toHaveBeenCalledWith(API_KEY, AUTH_TOKEN, APP_ID);
 		});
-	});
 
-	describe('followUser', () => {
-		it('should follow another user', async () => {
-			const targetSlug = 'user';
-			const sourceUserId = 'user123';
-			const targetUserId = 'user456';
+		it('should handle exception', async () => {
+			(connect as jest.Mock).mockImplementationOnce(() => { throw new Error(); });
 
-			await activityFeedService.followUser(
-				sourceUserId,
-				targetSlug,
-				targetUserId,
-			);
+			new ActivityFeedService(API_KEY, AUTH_TOKEN, APP_ID);
 
-			expect(connect().feed).toHaveBeenCalledWith('user', sourceUserId);
-			expect(connect().feed('targetSlug').follow).toHaveBeenCalled();
-		});
-	});
-
-	describe('addActivity', () => {
-		it('should add an activity to the feed', async () => {
-			const userFeed = activityFeedService.createUserFeed('user123');
-			const targetSlug = 'user';
-			const activity = {
-				actor: 'user123',
-				verb: 'post',
-				object: 'message:1',
-			};
-
-			await ActivityFeedService.addActivity(userFeed, activity);
-
-			expect(connect().feed).toHaveBeenCalled();
-			expect(connect().feed(targetSlug).addActivity).toHaveBeenCalledWith(
-				activity,
-			);
+			expect(spyParseError).toHaveBeenCalledTimes(1);
 		});
 	});
 
 	describe('getActivity', () => {
-		it('should retrieve activities from the feed', async () => {
-			const userFeed = activityFeedService.createUserFeed('user123');
-			const params = { limit: 10 };
-			const targetSlug = 'user';
+		it('should get activity', async () => {
+			const ID = 'test';
+			const result = await (new ActivityFeedService(API_KEY, AUTH_TOKEN, APP_ID)).getActivity(ID);
 
-			await ActivityFeedService.getActivity(userFeed, params);
+			expect(result).toBe(mockActivity);
+			expect(mockGetActivities).toHaveBeenCalledWith({ ids: [ID], withRecentReactions: true });
+		});
 
-			expect(connect().feed).toHaveBeenCalled();
-			expect(connect().feed(targetSlug).get).toHaveBeenCalledWith(params);
+		it('should handle exception', async () => {
+			mockGetActivities.mockImplementationOnce(() => { throw new Error(); });
+			const ID = 'test';
+			const result = await (new ActivityFeedService(API_KEY, AUTH_TOKEN, APP_ID)).getActivity(ID);
+
+			expect(result).toBe(undefined);
+			expect(spyParseError).toHaveBeenCalledTimes(1);
+		});
+	});
+
+	describe('addComment', () => {
+		it('should add comment', async () => {
+			const ID = 'test';
+			const TEXT = 'asd';
+			const result = await (new ActivityFeedService(API_KEY, AUTH_TOKEN, APP_ID)).addComment(ID, TEXT);
+
+			expect(result).toBe(true);
+			expect(mockAddComment).toHaveBeenCalledWith('comment', ID, { text: TEXT });
+		});
+
+		it('should handle exception', async () => {
+			mockAddComment.mockImplementationOnce(() => { throw new Error(); });
+			const result = await (new ActivityFeedService(API_KEY, AUTH_TOKEN, APP_ID)).addComment('test', 'asd');
+
+			expect(result).toBe(false);
+			expect(spyParseError).toHaveBeenCalledTimes(1);
 		});
 	});
 });
