01c1e5305cda4d2885afa1c4a4784630e7a6047d
Casey Dyer <dyersituations@gmail.com>
2024-03-04 16:24:38 -0800

    fix: add provide to commenting stories

--- @kiva/kv-components/vue/stories/KvCommentsContainer.stories.js
+++ @kiva/kv-components/vue/stories/KvCommentsContainer.stories.js
@@ -6,11 +6,20 @@ export default {
 	component: KvCommentsContainer,
 };
 
+const TEST_USER_NAME = 'Jess';
+const TEST_USER_IMAGE = 'https://www-0.development.kiva.org/img/s100/6b1a24092be3aaa22216874e644a4acf.jpg';
+
 const story = (args) => {
 	const template = (templateArgs, { argTypes }) => ({
 		props: Object.keys(argTypes),
 		components: { KvCommentsContainer },
 		setup() { return { args: templateArgs }; },
+		provide: {
+			fetchLenderInfo: () => Promise.resolve({
+				name: TEST_USER_NAME,
+				image: { url: TEST_USER_IMAGE },
+			}),
+		},
 		template: `
 			<div style="max-width: 800px;">
 				<KvCommentsContainer v-bind="args" />
@@ -28,8 +37,8 @@ export const Default = story({ comments });
 export const UserData = story(
 	{
 		comments,
-		userDisplayName: 'Jess',
-		userImageUrl: 'https://www-0.development.kiva.org/img/s100/6b1a24092be3aaa22216874e644a4acf.jpg',
+		userDisplayName: TEST_USER_NAME,
+		userImageUrl: TEST_USER_IMAGE,
 		userPublicId: 'Jess1234',
 	},
 );
--- @kiva/kv-components/vue/stories/KvCommentsList.stories.js
+++ @kiva/kv-components/vue/stories/KvCommentsList.stories.js
@@ -5,12 +5,20 @@ export default {
 	title: 'KvCommentsList',
 	component: KvCommentsList,
 };
+const TEST_USER_NAME = 'Jess';
+const TEST_USER_IMAGE = 'https://www-0.development.kiva.org/img/s100/6b1a24092be3aaa22216874e644a4acf.jpg';
 
 const story = (args) => {
 	const template = (templateArgs, { argTypes }) => ({
 		props: Object.keys(argTypes),
 		components: { KvCommentsList },
 		setup() { return { args: templateArgs }; },
+		provide: {
+			fetchLenderInfo: () => Promise.resolve({
+				name: TEST_USER_NAME,
+				image: { url: TEST_USER_IMAGE },
+			}),
+		},
 		template: `
 			<KvCommentsList v-bind="args" />
 		`,
@@ -26,8 +34,8 @@ export const Default = story({ comments });
 export const UserData = story(
 	{
 		comments,
-		userDisplayName: 'Jess',
-		userImageUrl: 'https://www-0.development.kiva.org/img/s100/6b1a24092be3aaa22216874e644a4acf.jpg',
+		userDisplayName: TEST_USER_NAME,
+		userImageUrl: TEST_USER_IMAGE,
 		userPublicId: 'Jess1234',
 	},
 );
