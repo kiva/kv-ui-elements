9bc34549eea7a0e47313732b5c5d232bd228fc95
Roger Gutierrez <rogelio.gutierrez@contractors.kiva.org>
2024-03-08 11:07:11 -0600

    feat: inline activity feed components added

---
+++ @kiva/kv-components/vue/stories/KvInlineActivityCard.stories.js
@@ -0,0 +1,69 @@
+import KvInlineActivityCard from '../KvInlineActivityCard.vue';
+
+export default {
+	title: 'KvInlineActivityCard',
+	component: KvInlineActivityCard,
+};
+
+const story = (args) => {
+	const template = (templateArgs, { argTypes }) => ({
+		props: Object.keys(argTypes),
+		components: { KvInlineActivityCard },
+		setup() { return { args: templateArgs }; },
+		template: `
+			<div style="max-width: 320px;">	
+				<KvInlineActivityCard v-bind="args" />
+			</div>
+		`,
+	});
+	template.args = args;
+	return template;
+};
+
+export const Default = story({
+	activity: {
+		lender: {
+			image: {
+				url: 'https://www.development.kiva.org/img/s100/26e15431f51b540f31cd9f011cc54f31.jpg',
+			},
+			name: 'Roger',
+		},
+		shareAmount: '125.00',
+	},
+});
+
+export const NoImage = story({
+	activity: {
+		lender: {
+			image: {
+				url: '',
+			},
+			name: 'Roger',
+		},
+		shareAmount: '75.00',
+	},
+});
+
+export const Anonymous = story({
+	activity: {
+		lender: {
+			image: {
+				url: 'https://www.development.kiva.org/img/s100/26e15431f51b540f31cd9f011cc54f31.jpg',
+			},
+			name: 'Anonymous',
+		},
+		shareAmount: '5.00',
+	},
+});
+
+export const DefaultProfile = story({
+	activity: {
+		lender: {
+			image: {
+				url: 'https://www.development.kiva.org/img/s100/4d844ac2c0b77a8a522741b908ea5c32.jpg',
+			},
+			name: 'Default Profile',
+		},
+		shareAmount: '25.00',
+	},
+});
