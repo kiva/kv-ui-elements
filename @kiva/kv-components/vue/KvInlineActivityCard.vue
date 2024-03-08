32c782a39d39100dcf24a7a322488d5e25b86d8b
Roger Gutierrez <94026278+roger-in-kiva@users.noreply.github.com>
2024-03-08 14:46:48 -0600

    feat: inline activity feed components added (#367)
    * feat: inline activity feed components added
    
    * fix: border radius changed

---
+++ @kiva/kv-components/vue/KvInlineActivityCard.vue
@@ -0,0 +1,60 @@
+<template>
+	<div
+		class="tw-flex tw-items-center tw-gap-1 tw-py-0.5 tw-px-1 tw-shadow-lg tw-rounded-sm"
+	>
+		<KvUserAvatar
+			:lender-image-url="lenderImageUrl"
+			:lender-name="lenderName"
+			is-small
+			class="activity-avatar"
+		/>
+		<p class="tw-text-base tw-whitespace-nowrap">
+			<span class="data-hj-suppress">{{ lenderName }}</span> contributed ${{ amountLent }}
+		</p>
+	</div>
+</template>
+
+<script>
+import { computed, toRefs } from 'vue-demi';
+import KvUserAvatar from './KvUserAvatar.vue';
+
+export default {
+	name: 'KvInlineActivityCard',
+	components: {
+		KvUserAvatar,
+	},
+	props: {
+		/**
+         * Activity data object
+         */
+		activity: {
+			type: Object,
+			default: () => ({}),
+		},
+	},
+	setup(props) {
+		const {
+			activity,
+		} = toRefs(props);
+
+		const lenderName = computed(() => activity?.value?.lender?.name ?? '');
+		const lenderImageUrl = computed(() => activity?.value?.lender?.image?.url ?? '');
+		const amountLent = computed(() => {
+			const amount = activity?.value?.shareAmount ?? '';
+			return parseFloat(amount).toFixed();
+		});
+
+		return {
+			lenderImageUrl,
+			lenderName,
+			amountLent,
+		};
+	},
+};
+</script>
+
+<style scoped lang="postcss">
+.activity-avatar, .activity-avatar >>> img, .activity-avatar >>> div {
+    @apply tw-w-4 tw-h-4;
+}
+</style>
---
+++ @kiva/kv-components/vue/stories/KvInlineActivityFeed.stories.js
@@ -0,0 +1,76 @@
+import KvInlineActivityFeed from '../KvInlineActivityFeed.vue';
+
+const activities = [
+	{
+		lender: {
+			id: 723174,
+			name: 'TonyB',
+			image: {
+				url: 'https://www-0.development.kiva.org/img/s100/6b1a24092be3aaa22216874e644a4acf.jpg',
+			},
+		},
+		shareAmount: '25.00',
+	},
+	{
+		lender: {
+			id: 723174,
+			name: 'Roger',
+			image: {
+				url: '',
+			},
+		},
+		shareAmount: '25.00',
+	},
+	{
+		lender: {
+			id: 723174,
+			name: 'Anonymous',
+			image: {
+				url: 'https://www-0.development.kiva.org/img/s100/6b1a24092be3aaa22216874e644a4acf.jpg',
+			},
+		},
+		shareAmount: '25.00',
+	},
+	{
+		lender: {
+			id: 723174,
+			name: 'Default user',
+			image: {
+				url: 'https://www-0.development.kiva.org/img/s100/4d844ac2c0b77a8a522741b908ea5c32.jpg',
+			},
+		},
+		shareAmount: '25.00',
+	},
+	{
+		lender: {
+			id: 723174,
+			name: 'Jessica',
+			image: {
+				url: 'https://www-0.development.kiva.org/img/s100/6b1a24092be3aaa22216874e644a4acf.jpg',
+			},
+		},
+		shareAmount: '25.00',
+	},
+];
+
+export default {
+	title: 'KvInlineActivityFeed',
+	component: KvInlineActivityFeed,
+};
+
+const story = (args) => {
+	const template = (templateArgs, { argTypes }) => ({
+		props: Object.keys(argTypes),
+		components: { KvInlineActivityFeed },
+		setup() { return { args: templateArgs }; },
+		template: `
+			<div style="max-width: 1200px;">	
+				<KvInlineActivityFeed v-bind="args" />
+			</div>
+		`,
+	});
+	template.args = args;
+	return template;
+};
+
+export const Default = story({ activities });
