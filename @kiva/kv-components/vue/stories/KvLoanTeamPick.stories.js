7cd14eae9a520b9a00359093250b862df78bc5c0
Roger Gutierrez <rogelio.gutierrez@contractors.kiva.org>
2024-03-15 13:44:02 -0600

    feat: team pick label added to loan card component

---
+++ @kiva/kv-components/vue/stories/KvLoanTeamPick.stories.js
@@ -0,0 +1,20 @@
+import KvLoanTeamPick from '../KvLoanTeamPick.vue';
+
+export default {
+	title: 'KvLoanTeamPick',
+	component: KvLoanTeamPick,
+};
+
+const story = (args) => {
+	const template = (_args, { argTypes }) => ({
+		props: Object.keys(argTypes),
+		components: { KvLoanTeamPick },
+		template: `
+			<kv-loan-team-pick  />
+		`,
+	});
+	template.args = args;
+	return template;
+};
+
+export const Default = story();
