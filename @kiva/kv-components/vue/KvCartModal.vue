79edbb9127259d9883b93ba1b564b3627452d90a
Christian Bedon <christian.bedon@contractors.kiva.org>
2024-07-11 21:44:59 -0500

    fix: modal size on desktop

--- @kiva/kv-components/vue/KvCartModal.vue
+++ @kiva/kv-components/vue/KvCartModal.vue
@@ -15,11 +15,7 @@
 		>
 			<div>
 				<div
-					class="
-						tw-absolute
-						tw-inset-0
-						md:tw-px-2
-					"
+					class="container"
 				>
 					<div
 						ref="kvCartModal"
@@ -336,12 +332,12 @@ export default {
 
 <style lang="postcss" scoped>
 .screen {
-	@apply tw-z-modal tw-absolute tw-inset-0;
+	@apply tw-z-modal tw-fixed tw-inset-0;
 	background-color: rgb(0, 0, 0, 0.2);
 }
 
 .modal {
-	@apply tw-bg-primary md:tw-absolute md:tw-right-0 tw-w-full md:tw-w-auto tw-rounded-b;
+	@apply tw-bg-primary md:tw-absolute md:tw-right-0 tw-w-full tw-rounded-b;
 	max-width: 30rem;
 	max-height: 90%;
 }
@@ -354,4 +350,19 @@ export default {
 .loan-image {
 	width: 3.75rem;
 }
+
+.container {
+	@apply tw-absolute tw-inset-0 md:tw-mt-9;
+}
+
+@screen md {
+	.container {
+		margin-right: 5%;
+	}
+
+	.modal {
+		max-width: 24.5rem;
+	}
+}
+
 </style>
