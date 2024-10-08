039bca45ac0b8fa2f902f4623af04da5d55a0a88
Eddie Ferrer <eddieferrer@users.noreply.github.com>
2024-10-08 14:39:43 -0600

    feat: new voting card (#466)
    * feat: new voting card
    
    * feat: change vote card v2 to composition api
    
    * fix: namespace css variable

---
+++ @kiva/kv-components/vue/stories/KvVotingCardV2.stories.js
@@ -0,0 +1,33 @@
+import KvVotingCardV2 from '../KvVotingCardV2.vue';
+
+export default {
+	title: 'KvVotingCardV2',
+	component: KvVotingCardV2,
+};
+
+const Template = (args) => ({
+	components: { KvVotingCardV2 },
+	setup() {
+		return { args };
+	},
+	template: `
+	<div style="max-width: 524px;">
+		<kv-voting-card-v2 v-bind="args" @vote="vote"/>
+	</div>
+  `,
+	methods: {
+		vote() {
+			console.log('vote');
+		},
+	},
+});
+
+export const Default = Template.bind({});
+Default.args = {
+	title: 'Women owned small businesses',
+	description: 'Just half of all women-owned businesses in the U.S. who apply for a loan are approved. Kiva provides access where others don\'t, creating more opportunities for women entrepreneurs.',
+	backgroundImageUrl: '//images.ctfassets.net/j0p9a6ql0rn7/3LZLziZYCl05YAhb6zlH6G/df770c45b790f065b8f3d11744ef1417/Lola__Oregon__United_States.jpeg',
+	percentage: 45,
+	showVoteButton: true,
+	showPercentage: true,
+};
