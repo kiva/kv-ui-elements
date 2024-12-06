4fbe5533d437af0d77b6d79b2f8ec8880f51c3dd
Christian Bedon <christian.bedon@contractors.kiva.org>
2024-12-06 15:12:31 -0500

    feat: sidesheet expand effect

--- @kiva/kv-components/vue/KvSideSheet.vue
+++ @kiva/kv-components/vue/KvSideSheet.vue
@@ -1,8 +1,8 @@
 <template>
 	<div
 		v-if="visible"
-		class="tw-block tw-absolute tw-inset-0
-            tw-bg-black tw-transition-all tw-duration-150 tw-z-modal"
+		class="tw-block tw-fixed lg:tw-mt-0 md:tw-absolute tw-inset-0
+            tw-bg-black tw-transition-all md:tw-duration-150 tw-z-modal"
 		:class="{
 			'tw-bg-opacity-0 tw-delay-300': !open,
 			'tw-bg-opacity-low': open,
@@ -12,11 +12,17 @@
 		<div
 			class="tw-absolute tw-right-0 tw-h-full tw-transition-all tw-duration-300 tw-bg-white"
 			:class="{
-				'tw-w-0 tw-p-0 tw-delay-200': !open,
-				'lg:tw-w-1/2 tw-w-full tw-p-2': open,
+				'tw-w-0 tw-p-0 tw-delay-200': !open && !expandEffect,
+				'lg:tw-w-1/2 tw-w-full tw-p-2': open && !expandEffect,
+				'tw-w-full tw-overflow-hidden !tw-bg-action': expandEffect
 			}"
+			:style="modalStyles"
+			@transitionend="onTransitionEnd"
 		>
-			<div class="tw-flex tw-justify-between">
+			<div
+				class="tw-flex tw-justify-between"
+				:class="{'tw-hidden': !open && expandEffect}"
+			>
 				<button
 					class="hover:tw-text-action-highlight"
 					@click="closeSideSheet"
@@ -89,6 +95,20 @@ export default {
 			type: String,
 			default: '',
 		},
+		/**
+		 * Give an effect of expand and shrink on the sheet
+		 */
+		expandEffect: {
+			type: Boolean,
+			default: false,
+		},
+		/**
+		 * Only used when expandEffect is true to set initial position where sheet will be expanded
+		 */
+		initialStyles: {
+			type: Object,
+			default: () => ({}),
+		},
 	},
 	emits: [
 		'side-sheet-closed',
@@ -98,15 +118,38 @@ export default {
 			visible,
 			kvTrackFunction,
 			trackEventCategory,
+			expandEffect,
+			initialStyles,
 		} = toRefs(props);
 
 		const open = ref(false);
+		const animating = ref(false);
+
+		const modalStyles = ref({});
+
+		if (expandEffect.value) {
+			modalStyles.value = {
+				...initialStyles,
+				transition: 'none',
+			};
+		}
 
 		const closeSideSheet = () => {
 			open.value = false;
 			kvTrackFunction.value(trackEventCategory.value, 'click', 'side-sheet-closed');
+
+			if (expandEffect.value) {
+				animating.value = true;
+
+				modalStyles.value = {
+					...initialStyles.value,
+					transition: 'all 0.7s ease-in-out',
+				};
+			}
+
 			setTimeout(() => {
 				emit('side-sheet-closed');
+				animating.value = false;
 			}, '700');
 		};
 
@@ -114,12 +157,36 @@ export default {
 			emit('go-to-link');
 		};
 
+		const onTransitionEnd = () => {
+			animating.value = false;
+		};
+
 		watch(visible, () => {
 			if (visible.value) {
 				setTimeout(() => {
 					open.value = true;
 				}, '300');
+
+				if (expandEffect.value) {
+					animating.value = true;
+					setTimeout(() => {
+						modalStyles.value = {
+							top: '0',
+							left: '0',
+							width: '100vw',
+							height: '100vh',
+							transition: 'all 0.5s ease-in-out',
+						};
+					}, 100);
+				}
 			}
+		});
+
+		watch(initialStyles, (newStyles) => {
+			modalStyles.value = {
+				...newStyles,
+				transition: 'none',
+			};
 		});
 
 		return {
@@ -128,6 +195,9 @@ export default {
 			open,
 			closeSideSheet,
 			goToLink,
+			onTransitionEnd,
+			modalStyles,
+			animating,
 		};
 	},
 };
