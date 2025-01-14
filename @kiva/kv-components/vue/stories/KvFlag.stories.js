fcc8484ff186039c8cccbbfe4ce2f7ac17422c55
Joshua Kiwiet-Pantaleoni <joshuak@kiva.org>
2025-01-13 16:54:25 -0800

    docs(KvFlag): add country names to stories

--- @kiva/kv-components/vue/stories/KvFlag.stories.js
+++ @kiva/kv-components/vue/stories/KvFlag.stories.js
@@ -23,15 +23,21 @@ const story = (args) => {
 };
 
 // SVG based on the country code
-export const InlineSvg = story({ country: 'TO' });
+export const InlineSvg = story({ country: 'TO', name: 'Tonga' });
 // SVG based on the country code with custom width
-export const InlineSvgCustomWidth = story({ country: 'DE', widthOverride: '40px' });
+export const InlineSvgCustomWidth = story({ country: 'DE', name: 'Germany', widthOverride: '40px' });
 
 // SVG Square based on the country code
-export const SquareInlineSvg = story({ country: 'ME', aspectRatio: '1x1', showName: true });
+export const SquareInlineSvg = story({
+	country: 'ME',
+	name: 'Montenegro',
+	aspectRatio: '1x1',
+	showName: true,
+});
 // SVG Square based on the country code with custom width
 export const SquareInlineSvgCustomWidth = story({
 	country: 'SA',
+	name: 'Saudi Arabia',
 	inlineSvg: true,
 	widthOverride: '40px',
 	aspectRatio: '1x1',
