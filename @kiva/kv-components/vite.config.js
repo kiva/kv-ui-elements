2aa4596f9e1b37d8ea442816c34425491f172107
Joshua Kiwiet-Pantaleoni <joshuak@kiva.org>
2025-01-16 16:53:43 -0800

    chore(kv-components): define peer and bundle dependencies

--- @kiva/kv-components/vite.config.js
+++ @kiva/kv-components/vite.config.js
@@ -12,29 +12,39 @@ export default defineConfig({
 	},
 	build: {
 		outDir: 'dist',
+		// Ensure that separate css files are created for each component with styles
 		cssCodeSplit: true,
+		// Enable Vite library mode
 		lib: {
 			entry: 'src/index.js',
 			formats: ['es'],
 			fileName: (format, entryName) => {
+				// Add .js or .cjs suffix to entryName based on format
 				const suffix = format === 'es' ? '.js' : '.cjs';
+				// Remove .vue extension from entryName for SFCs
 				if (entryName.slice(-4) === '.vue') {
 					return `${entryName.slice(0, -4)}${suffix}`;
 				}
+				// Return default entryName
 				return `${entryName}${suffix}`;
 			},
 		},
 	},
 	plugins: [
+		// Support Vue 3 single-file components
 		vue(),
+		// Make the output match the src file structure instead of bundling into one large file
 		noBundlePlugin({
+			// Dependencies that should be included in the final build. Update package.json bundledDependencies when changing this list.
 			internal: [
+				'aria-hidden',
 				'embla-carousel',
 				'embla-carousel-autoplay',
 				'embla-carousel-fade',
 				'popper.js',
 			],
 		}),
+		// Ensure assets are created in the dist folder when using Vite library mode
 		libAssetsPlugin({
 			name: '[name].[contenthash:8].[ext]',
 			outputPath: 'kvui',
