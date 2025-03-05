647ed4774936a27852ba74915b13f980de869ca3
Christian Bedon <christian.bedon@contractors.kiva.org>
2025-03-05 16:52:54 -0500

    feat: cart pill component for atb

---
+++ @kiva/kv-components/src/vue/KvCartPill.vue
@@ -0,0 +1,49 @@
+<template>
+	<div
+		class="tw-w-max tw-flex tw-items-center tw-gap-1 tw-px-1 tw-py-0.5 tw-rounded"
+		:class="{
+			'tw-text-small tw-bg-secondary': showBg,
+		}"
+	>
+		<slot name="icon"></slot>
+		<p class="tw-truncate">
+			{{ pillCopy }}
+		</p>
+	</div>
+</template>
+
+<script>
+
+import {
+	toRefs,
+	computed,
+} from 'vue';
+
+export default {
+	props: {
+		borrowerName: {
+			type: String,
+			default: '',
+		},
+		showBg: {
+			type: Boolean,
+			default: false,
+		},
+	},
+	setup(props) {
+		const {
+			borrowerName,
+		} = toRefs(props);
+
+		const pillCopy = computed(() => {
+			return borrowerName.value
+				? `Supporting ${borrowerName.value} will hit your next milestone!`
+				: 'Supporting this loan achieves a milestone!';
+		});
+
+		return {
+			pillCopy,
+		};
+	},
+};
+</script>
