19821123450fc0cc466c380a23f654ac7b65a1d4
Roger Gutierrez <rogelio.gutierrez@contractors.kiva.org>
2025-03-11 14:44:36 -0600

    feat: kvcartmodal content slot update

--- @kiva/kv-components/src/vue/stories/KvCartModal.stories.js
+++ @kiva/kv-components/src/vue/stories/KvCartModal.stories.js
@@ -43,3 +43,38 @@ export const Lightbox = DefaultTemplate.bind({});
 Lightbox.args = {
 	title: 'Added to Basket',
 };
+
+const ContentSlotTemplate = (args, { argTypes }) => ({
+	props: Object.keys(argTypes),
+	components: { KvCartModal, KvButton },
+	setup() { return { args }; },
+	template: `
+		<div>
+			<kv-button @click="isVisible = true;">Show cart modal</kv-button>
+			<p>The lightbox is visible: {{isVisible}}</p>
+
+			<kv-cart-modal
+				v-bind="args"
+				:visible="isVisible"
+				@cart-modal-closed="isVisible = false"
+			>
+				<template #content>
+					<div style="text-align: center; width: 100%;">
+						<h2>{{title}}</h2>
+						<p>Some content</p>
+					</div>
+				</template>
+			</kv-cart-modal>
+		</div>
+	`,
+	data() {
+		return {
+			isVisible: args.visible,
+		};
+	},
+});
+
+export const ContentSlot = ContentSlotTemplate.bind({});
+ContentSlot.args = {
+	title: 'Added to Basket',
+};
