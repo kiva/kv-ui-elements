2730acdcc55fc33949a850c508163e4e15f94c29
Chris Fowles <chris@niftic.agency>
2025-09-22 10:14:32 -0600

    feat: add addKivaCardToBasket wrapper to basketItems

--- @kiva/kv-shop/src/basketItems.ts
+++ @kiva/kv-shop/src/basketItems.ts
@@ -49,3 +49,71 @@ export async function setTipDonation({ amount, apollo, metadata }: SetTipDonatio
 
 	return data?.shop?.updateDonation;
 }
+
+// eslint-disable-next-line no-shadow
+export enum KivaCardDeliveryTypeEnum {
+	Email = 'email',
+	Lender = 'lender',
+	Postal = 'postal',
+	Print = 'print'
+}
+
+export interface KivaCardRecipientInput {
+	email?: string;
+	name?: string;
+	address?: string;
+	city?: string;
+	state?: string;
+	postalCode?: string;
+	country?: string;
+}
+
+export interface KivaCardInput {
+	deliveryType: KivaCardDeliveryTypeEnum;
+	giftAmount: string | number;
+	message?: string | null;
+	recipient: KivaCardRecipientInput;
+	senderEmail?: string | null;
+	senderName?: string | null;
+}
+
+export interface AddKivaCardToBasketOptions {
+	kivaCard: KivaCardInput,
+	apollo: ApolloClient<any>,
+}
+
+export interface AddKivaCardToBasketData {
+	shop: {
+		id: string,
+		addKivaCardToBasket: {
+			id: string,
+			basketItemType: string,
+			price: string,
+		} | null,
+	} | null,
+}
+
+export async function addKivaCardToBasket({ kivaCard, apollo }: AddKivaCardToBasketOptions) {
+	const giftAmount = numeral(kivaCard.giftAmount).format('0.00');
+	const data = await callShopMutation<AddKivaCardToBasketData>(apollo, {
+		awaitRefetchQueries: true,
+		mutation: gql`mutation AddKivaCardToBasket($kivaCard: KivaCardInput!, $basketId: String) {
+			shop (basketId: $basketId) {
+				id
+				addKivaCardToBasket(kivaCard: $kivaCard) {
+					id
+					basketItemType
+					price
+				}
+			}
+		}`,
+		variables: {
+			kivaCard: {
+				...kivaCard,
+				giftAmount,
+			},
+		},
+	});
+
+	return data?.shop?.addKivaCardToBasket;
+}
