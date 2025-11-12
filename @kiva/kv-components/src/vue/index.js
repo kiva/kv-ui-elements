6a61dd5a15b8903d018c481fa702bad118a13970
Matt Stover <mcs@mcstover.com>
2025-11-12 09:56:10 -0800

    fix: ensure KvLoanInfoCard has unique exports

--- @kiva/kv-components/src/vue/index.js
+++ @kiva/kv-components/src/vue/index.js
@@ -43,6 +43,7 @@ export { default as KvLoanBookmark } from './KvLoanBookmark.vue';
 export * from './KvLoanBookmark.vue';
 export { default as KvLoanCallouts } from './KvLoanCallouts.vue';
 export { default as KvLoanInfoCard } from './KvLoanInfoCard.vue';
+export * from './KvLoanInfoCard.vue';
 export { default as KvLoanProgressGroup } from './KvLoanProgressGroup.vue';
 export { default as KvLoanTag } from './KvLoanTag.vue';
 export * from './KvLoanTag.vue';
