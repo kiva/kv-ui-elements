720a03e1a9c084b7d5df9042dd96eecd572dc8c1
Joshua Kiwiet-Pantaleoni <joshuak@kiva.org>
2025-11-17 12:39:33 -0800

    test: remove commented-out setup code

--- @kiva/kv-components/tests/unit/jest-setup.js
+++ @kiva/kv-components/tests/unit/jest-setup.js
@@ -1,9 +1,5 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import '@testing-library/jest-dom';
 import { toHaveNoViolations } from 'jest-axe';
-// import { config } from '@vue/test-utils';
-
-// Vue Test Utils global configuration
-// config.global.config.warnHandler = () => {};
 
 expect.extend(toHaveNoViolations);
