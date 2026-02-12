01fd32bf46a9c27422f33dd6dcbca509d2117bde
Roger Gutierrez <rogelio.gutierrez@contractors.kiva.org>
2026-02-12 10:35:27 -0600

    fix: lint issues

--- @kiva/kv-components/src/vue/KvProgressCircle.vue
+++ @kiva/kv-components/src/vue/KvProgressCircle.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+	<div>
 		<!-- visually hidden, here for accessibility purposes -->
 		<progress
 			class="tw-sr-only"
@@ -53,6 +53,7 @@
 					<defs>
 						<!-- path the text should follow -->
 						<path
+							id="text_circle"
 							:d="`
 							M ${radius}, ${radius}
 							m -${radius}, 0
@@ -60,7 +61,6 @@
 							a ${radius},${radius} 0 1,1 -${radius * 2},0
 						`"
 							class="tw-text-transparent tw-fill-current tw-origin-center"
-							id="text_circle"
 							:style="{
 								'transform': `${textCircleTransform}`
 							}"
@@ -68,6 +68,7 @@
 
 						<!-- flipped path -->
 						<path
+							id="text_circle_flipped"
 							:d="`
 							M ${radius}, ${radius}
 							m -${radius}, 0
@@ -75,7 +76,6 @@
 							a ${radius},${radius} 0 1,0 -${radius * 2},0
 						`"
 							class="tw-text-transparent tw-fill-current tw-origin-center"
-							id="text_circle_flipped"
 							:style="{
 								'transform': `${textCircleTransform}`
 							}"
@@ -134,16 +134,16 @@ export default {
 		/**
          * Value of the progress circle, between 0 and 100.
          * */
-        value: {
+		value: {
 			type: Number,
 			required: true,
 			validator(val: number) {
 				return val >= 0 && val <= 100;
-			}
+			},
 		},
 		/**
 		 * Create an arc instead of a full circle. A percent.
-		 * */  
+		 * */
 		arcScale: {
 			type: Number,
 			default: 1,
@@ -160,14 +160,14 @@ export default {
 		 * */
 		strokeWidth: {
 			default: 8,
-			type: Number
+			type: Number,
 		},
 		/**
 		 * Whether to show the value as a number
 		 * */
 		showNumber: {
 			default: false,
-			type: Boolean
+			type: Boolean,
 		},
 		/**
 		 * Color of the progress circle
@@ -175,61 +175,61 @@ export default {
 		color: {
 			type: String,
 			default: 'brand',
-		}
+		},
 	},
-    setup(props) {
-        const radius = ref(100);
+	setup(props) {
+		const radius = ref(100);
 
-        const fontSize = computed(() => props.strokeWidth * 3);
-        
-        const normalizedRadius = computed(() => radius.value - (props.strokeWidth / 2));
-        
-        const circumference = computed(() => normalizedRadius.value * 2 * Math.PI);
-        
-        const strokeDashoffset = computed(() => circumference.value - (props.value / 100) * circumference.value * props.arcScale);
-        
-        const backgroundStrokeDashoffset = computed(() => circumference.value - 1 * circumference.value * props.arcScale);
-        
-        const circleTransform = computed(() => {
-            const offset = 90;
-            return `rotate(${offset + props.rotate}deg)`;
-        });
-        
-        const textCircleTransform = computed(() => {
-            const offset = -90;
-            return `rotate(${offset + props.rotate}deg)`;
-        });
-        
-        const flipText = computed(() => props.value > 75 || props.value < 25);
-        
-        const textDx = computed(() => {
-            if (flipText.value) {
-                return circumference.value - (props.value / 100) * circumference.value * props.arcScale;
-            }
-            return (props.value / 100) * circumference.value * props.arcScale;
-        });
-        
-        const textDy = computed(() => {
-            if (flipText.value) {
-                return 0;
-            }
-            return fontSize.value * 0.625;
-        });
+		const fontSize = computed(() => props.strokeWidth * 3);
 
-        return {
-            radius,
-            fontSize,
-            normalizedRadius,
-            circumference,
-            strokeDashoffset,
-            backgroundStrokeDashoffset,
-            circleTransform,
-            textCircleTransform,
-            flipText,
-            textDx,
-            textDy,
-        };
-    },
+		const normalizedRadius = computed(() => radius.value - (props.strokeWidth / 2));
+
+		const circumference = computed(() => normalizedRadius.value * 2 * Math.PI);
+
+		const strokeDashoffset = computed(() => circumference.value - (props.value / 100) * circumference.value * props.arcScale); // eslint-disable-line max-len
+
+		const backgroundStrokeDashoffset = computed(() => circumference.value - 1 * circumference.value * props.arcScale); // eslint-disable-line max-len
+
+		const circleTransform = computed(() => {
+			const offset = 90;
+			return `rotate(${offset + props.rotate}deg)`;
+		});
+
+		const textCircleTransform = computed(() => {
+			const offset = -90;
+			return `rotate(${offset + props.rotate}deg)`;
+		});
+
+		const flipText = computed(() => props.value > 75 || props.value < 25);
+
+		const textDx = computed(() => {
+			if (flipText.value) {
+				return circumference.value - (props.value / 100) * circumference.value * props.arcScale;
+			}
+			return (props.value / 100) * circumference.value * props.arcScale;
+		});
+
+		const textDy = computed(() => {
+			if (flipText.value) {
+				return 0;
+			}
+			return fontSize.value * 0.625;
+		});
+
+		return {
+			radius,
+			fontSize,
+			normalizedRadius,
+			circumference,
+			strokeDashoffset,
+			backgroundStrokeDashoffset,
+			circleTransform,
+			textCircleTransform,
+			flipText,
+			textDx,
+			textDy,
+		};
+	},
 };
 </script>
 
