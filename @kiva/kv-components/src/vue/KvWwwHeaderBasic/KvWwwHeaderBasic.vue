c3ab70bdc76ffc5c847b342218cce7a9a2bedb42
Matt Stover <matthews@kiva.org>
2026-05-27 09:38:38 -0600

    feat(KvWwwHeaderBasic): scaffold component folder and barrel export

---
+++ @kiva/kv-components/src/vue/KvWwwHeaderBasic/KvWwwHeaderBasic.vue
@@ -0,0 +1,28 @@
+<template>
+	<kv-theme-provider
+		tag="div"
+		class="tw-bg-primary"
+	>
+		<nav
+			class="tw-font-medium tw-relative"
+			:style="{ height: HEADER_HEIGHT }"
+		>
+			<kv-page-container class="tw-h-full" />
+		</nav>
+	</kv-theme-provider>
+</template>
+
+<script lang="ts">
+import KvThemeProvider from '#components/KvThemeProvider.vue';
+import KvPageContainer from '#components/KvPageContainer.vue';
+
+const HEADER_HEIGHT = '4rem';
+
+export default {
+	name: 'KvWwwHeaderBasic',
+	components: { KvThemeProvider, KvPageContainer },
+	setup() {
+		return { HEADER_HEIGHT };
+	},
+};
+</script>
